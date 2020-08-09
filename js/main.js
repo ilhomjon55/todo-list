143512bbb9a3a290c88d1ee3f6dbb3be6c8211e6
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-08-09 19:16:45 +0500

    Add Speech recognition JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+const rec = new webkitSpeechRecognition()
+rec.lang = 'en-US';
+rec.onend = function () {
+   console.log("Connection is ended!")
+}
+rec.onresult = function (event) {
+   const command = event.results[0][0].transcript
+   console.log(command)
+
+   // DECLARING AND ADDING LI
+   const tasks = document.createElement('LI')
+   task_box.appendChild(tasks)
+
+
+   // DECLARING AND ADDING P
+   const task_text = document.createElement('P')
+   task_text.textContent = command
+   tasks.appendChild(task_text)
+   task_name.value = ''
+
+   // DECLARING AND ADDING BUTTON
+   const removeBut = document.createElement('button')
+   tasks.appendChild(removeBut)
+
+   // ADDING REMOVE FUNCTION TO BUTTON
+   removeBut.onclick = function () {
+      tasks.remove()
+   }
+
+   if (command === "delete all") {
+      task_box.remove()
+   }
+}
+rec.onerror = function () {
+   console.log("Error ocured")
+}
+window.onkeyup = function (e) {
+   if (e.keyCode === 32) {
+      rec.start()
+
+   }
+}
+
+
 task_name.onkeyup = function (event) {
 
    if (event.keyCode === 13 && task_name.value.length > 0) {
@@ -13,7 +57,7 @@ task_name.onkeyup = function (event) {
       task_name.value = ''
 
       // DECLARING AND ADDING BUTTON
-      const removeBut = document.createElement('button')
+      const removeBut = document.createElement('BUTTON')
       tasks.appendChild(removeBut)
 
       // ADDING REMOVE FUNCTION TO BUTTON
